1e4e43016e144b422dd1941a1b2213f2041ec991
VcelistMC <60217976+VcelistMC@users.noreply.github.com>
2021-06-03 21:34:54 +0200

    added player functions

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,23 @@
 package src;
 
-public class Player extends User{
+import java.util.ArrayList;
 
+public class Player extends User {
+    private ArrayList<Reservation> reservations;
+
+    Player() {
+        super();
+    }
+
+    Player(String name, int age, String email, String password, eWallet wallet) {
+        super(name, age, email, password, wallet);
+    }
+
+    public ArrayList<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void addReservation(Reservation reservation) {
+        this.reservations.add(reservation);
+    }
 }
